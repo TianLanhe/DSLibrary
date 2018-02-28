7f5c7341765e42c31c4c2716394a8b34b257feca
ZhenyuHe <849416763@qq.com>
2018-02-28 20:08:05 +0800

    add static circular single linked list

---
+++ list/StaticCLinkList.h
@@ -0,0 +1,18 @@
+#ifndef STATIC_CIRCULAR_SINGLE_LINK_LIST_H
+#define STATIC_CIRCULAR_SINGLE_LINK_LIST_H
+
+#include "CLinkList.h"
+
+DSLIB_BEGIN
+
+template < typename T, size_t N >
+class StaticCLinkList : public CLinkList<T, StaticMemoryManager<SNode<T>, N + 1> > {
+
+public:
+	size_type capacity() { return N; }
+
+};
+
+DSLIB_END
+
+#endif // !STATIC_CIRCULAR_SINGLE_LINK_LIST_H
