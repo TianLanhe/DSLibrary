f6495e98834993d3aaa625de5329937c47b53287
ZhenyuHe <849416763@qq.com>
2018-03-01 20:05:57 +0800

    add static doubly linked list

---
+++ list/linked_list/StaticDLinkList.h
@@ -0,0 +1,18 @@
+#ifndef STATIC_DOUBLY_LINKED_LIST_H
+#define STATIC_DOUBLY_LINKED_LIST_H
+
+#include "DLinkList.h"
+
+DSLIB_BEGIN
+
+template < typename T, size_t N >
+class StaticDLinkList : public DLinkList<T, StaticMemoryManager<DNode<T>, N + 1>> {
+
+public:
+	size_type capacity() { return N; }
+
+};
+
+DSLIB_END
+
+#endif // !STATIC_DOUBLY_LINKED_LIST_H
